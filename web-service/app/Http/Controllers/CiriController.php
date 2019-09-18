7cbf32740978733519f309c34f71b8f6d4a8ad73
Fakhri Nurfauzan <fakhrinf@hotmail.com>
2019-09-18 09:23:35 +0700

    ciri ciri rest api

--- web-service/app/Http/Controllers/CiriController.php
+++ web-service/app/Http/Controllers/CiriController.php
@@ -3,33 +3,98 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\CiriMazhabModel;
+use App\Ciricirimodel;
 
 class CiriController extends Controller
 {
     //
 
-    public function addciri()
+    public function addciri(Request $request)
     {
-        # code...
+        try {
+
+            $kode = $request->kode;
+            $ciri = $request->ciriciri;
+
+            $data = new CiriciriModel();
+            $data->ciriciri = $ciri;
+            $data->kode_ciri = $kode;
+
+            $data->save();
+
+            return response()->json(['message' => "Ciri with id:{$id} added."], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
     }
 
-    public function editciri($id)
+    public function editciri(Request $request, $id)
     {
-        # code...
+        try {
+
+            $kode = $request->kode;
+            $ciri = $request->ciriciri;
+
+            $data = CiriciriModel::find($id);
+
+            if(empty($data)) {
+                return response()->json(['message' => "data not found."], 404);
+            }else{
+                $data->ciriciri = $ciri;
+                $data->kode_ciri = $kode;
+    
+                $data->save();
+    
+                return response()->json(['message' => "Ciri with id:{$id} updated."], 200);
+            }
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
     }
 
     public function deleteciri($id)
     {
-        # code...
+        try {
+
+            $ciri = CiriciriModel::find($id);
+            if(empty($ciri)){
+                return response()->json(['message' => "data not found."], 404);
+            }else{
+                $ciri->delete();
+                $cirimazhab = CiriMazhab::where('ciri_id', $id)->delete();
+
+                return response()->json(['message' => "Ciri with id:{$id} deleted."], 200);
+            }
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
     }
 
     public function getallciri()
     {
-        # code...
+        try {
+            
+            $data = CiriciriModel::all();
+            return response()->json(['data' => $data], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
     }
 
     public function getciribyid($id)
     {
-        # code...
+        try {
+
+            $data = CiriciriModel::find($id);
+            return response()->json(['data' => $data], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
     }
 }
