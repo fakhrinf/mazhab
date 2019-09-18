b84186cd65f56b4f0a18d3ce24349f2528a6e752
Fakhri Nurfauzan <fakhrinf@hotmail.com>
2019-09-18 13:16:07 +0700

    about page

---
+++ mobile/lib/about.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class About extends StatefulWidget {
+  @override
+  _AboutState createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.6, 0.7],
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.green[200],
+              Colors.green[500],
+              Colors.green[600],
+            ],
+          ),
+        ),
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: MediaQuery.of(context).size.height * 0.5,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16)
+            ),
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/img/search.png"),
+                Divider(height: 10, color: Colors.transparent,),
+                Text("CEK MAZHAB APPS"),
+                Divider(height: 100, color: Colors.transparent,),
+                Text("Version: 1.0"),
+                Divider(height: 70, color: Colors.transparent,),
+                Text("Icons by:"),
+                FlatButton(
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {},
+                  child: Text("Icon8", style: TextStyle(color: Colors.blue)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:mazhab/about.dart';
+import 'package:mazhab/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,87 +28,96 @@ class _MainmenuState extends State<Mainmenu> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        elevation: 0,
-      ),
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 100,
-                color: Colors.green,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset("assets/img/search.png"),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 18.0),
-                        child: Text("CEK MAZHAB!", style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold)),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(32),
-                child: Column(
-                  children: <Widget>[
-                    RaisedButton(
-                      color: Colors.white,
-                      padding: EdgeInsets.all(16),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-                      onPressed: () {},
-                      child: Row(
-                        children: <Widget>[
-                          Image.asset("assets/img/konsultasi.png"),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 18.0),
-                            child: Text("Konsultasi", style: TextStyle(fontSize: 24),),
-                          )
-                        ],
-                      ),
-                    ),
-                    Divider(height: 20, color: Colors.transparent,),
-                    RaisedButton(
-                      color: Colors.white,
-                      padding: EdgeInsets.all(16),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-                      onPressed: () {},
-                      child: Row(
-                        children: <Widget>[
-                          Image.asset("assets/img/about.png"),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 18.0),
-                            child: Text("Info", style: TextStyle(fontSize: 24),),
-                          )
-                        ],
-                      ),
-                    ),
-                    Divider(height: 20, color: Colors.transparent,),
-                    RaisedButton(
-                      color: Colors.white,
-                      padding: EdgeInsets.all(16),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-                      onPressed: () {
-                        Navigator.push(context, new MaterialPageRoute(builder: (context) => LoginPage()));
-                      },
-                      child: Row(
-                        children: <Widget>[
-                          Image.asset("assets/img/login.png"),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 18.0),
-                            child: Text("Login", style: TextStyle(fontSize: 24),),
-                          )
-                        ],
-                      ),
-                    ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  // Where the linear gradient begins and ends
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  // Add one stop for each color. Stops should increase from 0 to 1
+                  stops: [0.1, 0.6, 0.7],
+                  colors: [
+                    // Colors are easy thanks to Flutter's Colors class.
+                    Colors.green[200],
+                    Colors.green[500],
+                    Colors.green[600],
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset("assets/img/search.png"),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Text("CEK MAZHAB!", style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold)),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(32),
+              child: Column(
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                    onPressed: () {},
+                    child: Row(
+                      children: <Widget>[
+                        Image.asset("assets/img/konsultasi.png"),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          child: Text("Konsultasi", style: TextStyle(fontSize: 24),),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(height: 20, color: Colors.transparent,),
+                  RaisedButton(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => About())),
+                    child: Row(
+                      children: <Widget>[
+                        Image.asset("assets/img/about.png"),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          child: Text("Info", style: TextStyle(fontSize: 24),),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(height: 20, color: Colors.transparent,),
+                  RaisedButton(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                    onPressed: () {
+                      Navigator.push(context, new MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: Row(
+                      children: <Widget>[
+                        Image.asset("assets/img/login.png"),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 18.0),
+                          child: Text("Login", style: TextStyle(fontSize: 24),),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
