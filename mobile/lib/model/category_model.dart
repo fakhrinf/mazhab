f2a6c059b67e6a71ae21a310cbff1c628e9be651
Fakhri Nurfauzan <fakhrinf@hotmail.com>
2019-10-24 16:07:43 +0700

    category dart model

---
+++ mobile/lib/model/category_model.dart
@@ -0,0 +1,81 @@
+import 'package:http/http.dart' as http;
+import 'package:mazhab/helper/api.dart';
+import 'dart:convert' as convert;
+
+class CategoryModel {
+  int id;
+  String category;
+  String createdAt;
+  String updatedAt;
+
+  CategoryModel({this.id, this.category, this.createdAt, this.updatedAt});
+
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    category = json['category'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['category'] = this.category;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
+  }
+
+  static Future<List<CategoryModel>> getCategories() async {
+    final response = await http.get(Api.url('category'));
+    if(response.statusCode == 200) {
+      List<CategoryModel> data = new List();
+      final json = convert.jsonDecode(response.body);
+      final category = json['data'];
+      category.forEach((e) {
+        data.add(new CategoryModel.fromJson(e));
+      });
+
+      return data;
+    }else{
+      throw Exception("Failed load data.");
+    }
+  }
+
+  static Future<String> addCategory(String category) async {
+    final response = await http.post(Api.url('category'), body: {
+      category: category
+    });
+    
+    final data = convert.jsonDecode(response.body);
+    
+    if(response.statusCode == 200) {
+      return data['message'];
+    }else{
+      throw Exception(data['message']);
+    }
+  }
+
+  static Future<String> editCategory(int id, String category) async {
+    final response = await http.put(Api.url('category/$id'), body: {
+      category: category
+    });
+    
+    final data = convert.jsonDecode(response.body);
+    
+    if(response.statusCode == 200) {
+      return data['message'];
+    }else{
+      throw Exception(data['message']);
+    }
+  } 
+
+  static Future<bool> deleteCategory(int id) async {
+    final response = await http.delete(Api.url("category/$id"));
+    if(response.statusCode == 200) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
