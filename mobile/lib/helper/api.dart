d297ebbef487e22f9d54b6fcf294830085b799db
Fakhri Nurfauzan <fakhrinf@hotmail.com>
2019-12-18 16:38:48 +0700

    order output naive bayes by presentase

--- mobile/lib/helper/api.dart
+++ mobile/lib/helper/api.dart
@@ -1,6 +1,6 @@
 class Api {
   
-  static const BASE_URL = "http://192.168.100.8:8000";
+  static const BASE_URL = "http://192.168.10.59:8000";
 
   static String url(route) {
     return "$BASE_URL/$route";
